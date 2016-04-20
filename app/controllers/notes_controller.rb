4ab4fc201728ca684d05429b65f5d268f6f80929
Robert Joseph Cox Jr <robert@studiorclv.com>
2016-04-19 23:45:15 -0700

    Initial note functionality in place

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,13 @@
 class NotesController < ApplicationController
   def index
-
+    @notes = Note.all
   end
 
   def create
+    @note = Note.new(params.require(:note).permit(:title, :text))
 
+    @note.save
+    redirect_to @note
   end
 
   def new
@@ -16,7 +19,7 @@ class NotesController < ApplicationController
   end
 
   def show
-
+    @note = Note.find(params[:id])
   end
 
   def update
