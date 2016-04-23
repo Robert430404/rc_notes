13236664bac2d3f59c28f09c5a1e5922e24791d9
Robert Joseph Cox Jr <robert@studiorclv.com>
2016-04-22 22:18:48 -0700

    Additional styles for the login page, and made login root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   # Home Route
-  root 'home#index'
-
+  # root 'home#index'
+  devise_scope :user do
+    root :to => 'devise/sessions#new'
+  end
   # GET Routes
 
   # Resource routes
   resources :notes
 
-  # Authentication Routes  
+  # Authentication Routes
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
