399d7102bf196d6e60fc6e9a4dfcc828924a8026
Robert Joseph Cox Jr <robert@studiorclv.com>
2016-04-30 13:20:17 -0700

    Added Generate And Delete fucntions

--- app/controllers/apikeys_controller.rb
+++ app/controllers/apikeys_controller.rb
@@ -2,6 +2,18 @@ class ApikeysController < ApplicationController
   before_action :authenticate_user!
 
   def new
+  	ApiKey.create
 
+  	flash[:notice] = "API Key successfully created"
+  	redirect_to setting_url
+  end
+
+  def destroy
+    @key = ApiKey.find(params[:id])
+
+    @key.destroy()
+
+  	flash[:notice] = "API Key successfully deleted"
+  	redirect_to setting_url
   end
 end
